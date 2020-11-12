71f06d6331b584f4a0568b8ad30bf2af582ae2ed
hagirs <49359800+hagirs@users.noreply.github.com>
2020-11-12 06:35:31 -0800

    Create while_loop.ino

---
+++ 6. Pengenalan Perulangan/while_loop.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  /* Perngulangan For */
+ int i = 0;
+ while(i < 100){
+  Serial.println(i);
+  i++;
+ }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
