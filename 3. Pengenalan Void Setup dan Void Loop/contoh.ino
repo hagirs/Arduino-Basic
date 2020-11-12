737396f1be25cc7630844ee00df1861fe3d1f76b
hagirs <49359800+hagirs@users.noreply.github.com>
2020-11-12 06:08:43 -0800

    Update contoh.ino

--- 3. Pengenalan Void Setup dan Void Loop/contoh.ino
+++ 3. Pengenalan Void Setup dan Void Loop/contoh.ino
@@ -6,6 +6,6 @@ void setup(){
 }
 void loop(){
 /* Program akan dijalankan berulang kali setelah fungsi void setup dijalankan,fungsi ini digunakan untuk mengeksekusi dan menjalankan program yang sudah dibuat */
-  Serial.println("aku ada di void setup");
+  Serial.println("aku ada di void loop");
 }
 
