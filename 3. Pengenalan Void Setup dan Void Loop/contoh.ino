6f026dc6e0bcfb40feb916fd3eab0014df28604e
hagirs <49359800+hagirs@users.noreply.github.com>
2020-11-12 05:08:35 -0800

    Update contoh.ino

--- 3. Pengenalan Void Setup dan Void Loop/contoh.ino
+++ 3. Pengenalan Void Setup dan Void Loop/contoh.ino
@@ -1,2 +1,7 @@
-void setup(){
 
+void setup(){
+/* Program akan dijalankan sekali saja setiap arduino di mulai, fungsi ini digunakan untuk menginisialisasi variabel mendekralasikan pin , mengunakan library dll. */
+}
+void loop(){
+/* Program akan dijalankan berulang kali setelah fungsi void setup dijalankan,fungsi ini digunakan untuk mengeksekusi dan menjalankan program yang sudah dibuat */ 
+}
