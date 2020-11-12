518c21636714449c8935dca6dfd39a51dc2a93bf
hagirs <49359800+hagirs@users.noreply.github.com>
2020-11-12 06:08:23 -0800

    Update contoh.ino

--- 3. Pengenalan Void Setup dan Void Loop/contoh.ino
+++ 3. Pengenalan Void Setup dan Void Loop/contoh.ino
@@ -1,7 +1,11 @@
 
 void setup(){
 /* Program akan dijalankan sekali saja setiap arduino di mulai, fungsi ini digunakan untuk menginisialisasi variabel mendekralasikan pin , mengunakan library dll. */
+  Serial.begin(9600);
+  Serial.println("aku ada di void setup");
 }
 void loop(){
-/* Program akan dijalankan berulang kali setelah fungsi void setup dijalankan,fungsi ini digunakan untuk mengeksekusi dan menjalankan program yang sudah dibuat */ 
+/* Program akan dijalankan berulang kali setelah fungsi void setup dijalankan,fungsi ini digunakan untuk mengeksekusi dan menjalankan program yang sudah dibuat */
+  Serial.println("aku ada di void setup");
 }
+
