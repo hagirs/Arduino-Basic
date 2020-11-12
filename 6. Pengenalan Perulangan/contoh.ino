322965bf4b07a16263b15c840187622e3529cefb
hagirs <49359800+hagirs@users.noreply.github.com>
2020-11-12 06:28:58 -0800

    Create contoh.ino

---
+++ 6. Pengenalan Perulangan/contoh.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  /* Perngulangan For */
+  int i;
+  for(i = 0;i < 100;i++){
+    Serial.println(i); 
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
